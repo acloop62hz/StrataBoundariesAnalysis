8081505e9f3cffd50c5f05cce985b13ad8c20f7e
Syx <56076629+acloop62hz@users.noreply.github.com>
2022-12-31 09:51:43 -0800

    Delete 02.GetWcds.sh

--- 02.GetWcds.sh
+++
@@ -1,3 +0,0 @@
-spe=$1
-#02.get_Wcds
-/ldfssz1/ST_DIVERSITY/PUB/USER/fangqi/local/bio-tool/cufflinks-2.2.1/gffread -g /hwfssz5/ST_EARTH/P18Z10200N0100/c-shenyuxuan/xiaoman/data/01.genome/$spe.genomic.fa /hwfssz5/ST_EARTH/P18Z10200N0100/c-shenyuxuan/source/gene_annotation/Wgff/$spe.w.gff -x /hwfssz5/ST_EARTH/P18Z10200N0100/c-shenyuxuan/source/gene_annotation/Wcds/$spe.w.cds
